8109be53f665e8dad37df34958fa9833935561a0
divadnauj-GB <juan.david.guerrero.balaguera@gmail.com>
2026-08-13 23:11:11 -0500

    added priority encoder

---
+++ Examples/3-Encoders/priority-encoder/priority_encoder_tb.v
@@ -0,0 +1,28 @@
+module priority_encoder_tb();
+
+reg [3:0] IN;
+wire [1:0] Y;
+wire Vld;
+
+priority_encoder DUT(
+    .A(IN),
+    .Y(Y),
+    .Vld(Vld)
+);
+
+
+initial begin
+    $dumpfile("sim.vcd"); 
+    $dumpvars;
+ end
+
+integer i;
+initial begin
+    for(i=0;i<16;i=i+1) begin
+        IN = i[3:0];
+        #10;
+    end
+    $finish;
+end
+
+endmodule
